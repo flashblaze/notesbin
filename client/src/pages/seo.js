61b984bb76ec8e7217b58d255cd73ac62995be84
FlashBlaze <neerajlagwankar@gmail.com>
2021-02-06 22:05:57 +0530

    feat[client]: SEO - Add favicon and open graph images

--- client/src/pages/seo.js
+++ client/src/pages/seo.js
@@ -22,10 +22,17 @@ const SEO = ({ slug = "", description = "" }) => {
         <meta property="og:url" content={ogURL} />
         <meta property="og:description" content={content} />
         <meta property="og:title" content={content} />
+        <meta property="og:image" content="https://notesbin.xyz/notesbin_og.png" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:creator" content="https://twitter.com/neeraj_artx" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content="Share notes with anyone, anywhere" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
     </div>
   );
